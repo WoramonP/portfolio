bb7171ed5321b3b8f29a5f9c9548524f96721f42
Woramon P <100491613+WoramonP@users.noreply.github.com>
2022-03-12 12:40:23 -0700

    RSA and AES encrypt, decrypt

---
+++ Encryption/AES_encrypt_decrypt.py
@@ -0,0 +1,33 @@
+"""
+    The following code generates a new AES128 key, encrypts a unicode string, and decrypts it.
+    Author: Woramon P.
+    Date: 3/12/22
+"""
+
+# pip install pycryptodome
+
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
+# generate a new AES128 key (16 bytes)
+AES_bytes = 16
+secret_key = get_random_bytes(AES_bytes)
+cipher = AES.new(secret_key, AES.MODE_ECB)  # ECB is not strong
+
+# encrypt a string
+string_to_encrypt = 'test STRING 123 AES กขค 测试'  # this is a unicode string
+print(type(string_to_encrypt), "length:", len(string_to_encrypt), string_to_encrypt)
+
+data_to_encrypt = pad(string_to_encrypt.encode('utf-8'), AES_bytes)  # convert to bytes and pad
+encrypted = cipher.encrypt(data_to_encrypt)
+print(type(encrypted), "length:", len(encrypted), encrypted)
+
+# decrypt a string
+decrypted = cipher.decrypt(encrypted)
+print(type(decrypted), "length:", len(decrypted), decrypted)
+
+decrypted_string = unpad(decrypted, AES_bytes).decode('utf-8')  # unpad and convert to string
+print(type(decrypted_string), "length:", len(decrypted_string), decrypted_string)
+
+assert(decrypted_string == string_to_encrypt)
---
+++ Encryption/RSA_encrypt_decrypt.py
@@ -0,0 +1,37 @@
+"""
+    The following code generates a new RSA key, encrypts a unicode string, and decrypts it.
+    Author: Woramon P.
+    Date: 3/12/22
+"""
+
+# pip install pycryptodome
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Cipher import PKCS1_OAEP
+
+# generate a new RSA key
+random_generator = Random.new().read
+bits = 2048
+
+private_key = RSA.generate(bits, random_generator)  # generate a private key
+public_key = private_key.publickey()  # get a public key of this key pair
+
+# encrypt a string, using public key
+string_to_encrypt = 'test STRING 123 RSA กขค 测试'  # this is a unicode string
+print(type(string_to_encrypt), "length:", len(string_to_encrypt), string_to_encrypt)
+
+data_to_encrypt = string_to_encrypt.encode('utf-8')  # convert to bytes
+cipher_rsa_public = PKCS1_OAEP.new(public_key)  # get a cipher object to perform encryption
+encrypted = cipher_rsa_public.encrypt(data_to_encrypt)  # encrypt
+print(type(encrypted), "length:", len(encrypted), encrypted)
+
+# decrypt a string
+cipher_rsa_private = PKCS1_OAEP.new(private_key)  # get a cipher object to perform decryption
+decrypted = cipher_rsa_private.decrypt(encrypted)  # decrypt
+print(type(decrypted), "length:", len(decrypted), decrypted)
+
+decrypted_string = decrypted.decode('utf-8')  # convert to string
+print(type(decrypted_string), "length:", len(decrypted_string), decrypted_string)
+
+assert(decrypted_string == string_to_encrypt)
